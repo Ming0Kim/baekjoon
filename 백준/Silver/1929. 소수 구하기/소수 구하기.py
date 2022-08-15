3df4dda4d023db07c1910a00dfd7273cd24bf20c
Ming0Kim <109322004+Ming0Kim@users.noreply.github.com>
2022-08-16 00:27:05 +0900

    [Silver III] Title: 소수 구하기, Time: 3800 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -0,0 +1,14 @@
+import sys
+M, N = map(int, sys.stdin.readline().split())
+
+def thtn(num):
+    if num == 1:
+        return False
+    for i in range(2, int(num**0.5)+1):
+        if num % i == 0:
+            return False
+    return True
+
+for j in range(M, N+1):
+    if thtn(j):
+        print(j)
