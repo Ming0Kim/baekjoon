19ecddf4b328c4d4b00803299a1d7c588b3557e1
Ming0Kim <109322004+Ming0Kim@users.noreply.github.com>
2022-08-26 16:03:17 +0900

    Delete 백준/Bronze/5622. 다이얼 directory

--- 백준/Bronze/5622. 다이얼/다이얼.py
+++
@@ -1,9 +0,0 @@
-dial = ['ABC', 'DEF', 'GHI', 'JKL', 'MNO', 'PQRS', 'TUV', 'WXYZ']
-string = input()
-
-num = 0
-for i in string:
-    for j in dial:
-        if i in j:
-            num += dial.index(j)+3
-print(num)
