2d450d02b4623b276cd8114641ab7eb466158a2f
Ming0Kim <109322004+Ming0Kim@users.noreply.github.com>
2022-09-29 23:44:48 +0900

    [Silver III] Title: N과 M (1), Time: 208 ms, Memory: 119024 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,16 +1,15 @@
-def f(i, k, r):
-  if i == r:
-    print(*p)
-  else:
-    for j in range(k):
-      if used[j] == 0:
-        used[j] = 1
-        p[i] = a[j]
-        f(i+1, k, r)
-        used[j] = 0
-  
-N, R = map(int, input().split())
-a = [i for i in range(1, N+1)]
-used = [0] * N
-p = [0] * R
-f(0, N, R)
+n, m = list(map(int, input().split()))
+
+s = []
+
+def dfs():
+    if len(s)==m:
+        print(*s)
+        return
+    for i in range(1, n+1):
+        if i not in s:
+            s.append(i)
+            dfs()
+            s.pop()
+
+dfs()
